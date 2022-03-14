9c201a6252f3cc98a6c867ce0376f61567eb50bb
LeonKln <101433601+LeonKln@users.noreply.github.com>
2022-03-14 18:33:19 +0100

    Delete code.js

--- code.js
+++
@@ -1,9 +0,0 @@
-/* Apre di aprire la navBar */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Chiude la navBar */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
