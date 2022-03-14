2d7145e1ce690d53af5e9db081713ef5e3221601
LeonKln <101433601+LeonKln@users.noreply.github.com>
2022-03-14 18:34:10 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+/* Apre di aprire la navBar */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Chiude la navBar */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
